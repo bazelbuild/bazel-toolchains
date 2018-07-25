0e55235ab9d58851d5f42a571462f4cc37e3aee1
arthurrab <arthurrab@google.com>
2018-07-25 16:00:25 -0400

    Moved bazel clean to main, and formatting

--- container/build.py
+++ container/build.py
@@ -13,25 +13,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """usage: build.py -d TYPE [-p PROJECT] [-c CONTAINER] [-t TAG]
+
                    -v BAZEL_VERSION [-a] [-b BUCKET] [-h] [-m MAP] [-l]
 
-Builds the fully-loaded container, with Google Cloud Container Builder or locally.
+Builds the fully-loaded container, with Google Cloud Container Builder or
+locally.
 
-IF THIS SCRIPT IS CALLED FROM OUTSIDE OF THE BAZEL-TOOLCHAINS REPO, THE BAZEL-TOOLCHAINS REPO
-MUST BE A SUBDIRECTORY OF THE OUTER PROJECT. OUTER PROJECT MUST ALSO HAVE bazel_toolchains AS
-A DEPENDENCY
+IF THIS SCRIPT IS CALLED FROM OUTSIDE OF THE BAZEL-TOOLCHAINS REPO, THE
+BAZEL-TOOLCHAINS REPO MUST BE A SUBDIRECTORY OF THE OUTER PROJECT. OUTER PROJECT
+MUST ALSO HAVE bazel_toolchains AS A DEPENDENCY
 Ex:
 cd <your project with your own build targets>
 git clone https://github.com/bazelbuild/bazel-toolchains.git
 python bazel-toolchains/container/build.py [args]
 
-Note: a file path passed to the -m param must point to a file in the form descibed below
+Note: a file path passed to the -m param must point to a file in the form
+descibed below
 (except TYPE_TARBALL_MAP is not required if the -b arg is not used)
 
 To build with Google Cloud Container Builder:
-$ python build.py -p my-gcp-project -d {container_type} -c {container_name} -t latest -b my_bucket
+$ python build.py -p my-gcp-project -d {container_type} -c {container_name} -t
+latest -b my_bucket
 will produce docker images in Google Container Registry:
     gcr.io/my-gcp-project/{container_type}:latest
 and the debian packages installed will be packed as a tarball and stored in
@@ -77,12 +80,9 @@ import sys
 LATEST_BAZEL_VERSION = "0.15.0"
 
 SUPPORTED_TYPES = [
-                    "rbe-debian8", 
-                    "rbe-debian9",
-                    "rbe-ubuntu16_04",
-                    "ubuntu16_04-bazel",
-                    "ubuntu16_04-bazel-docker"
-                  ]
+    "rbe-debian8", "rbe-debian9", "rbe-ubuntu16_04", "ubuntu16_04-bazel",
+    "ubuntu16_04-bazel-docker"
+]
 
 # ALL FILES USED IN -m TO OVERRIDE TARGETS MUST BE IN THIS FORM
 # =========== STARTING HERE ===========
@@ -128,19 +128,27 @@ assert set(SUPPORTED_TYPES) \
             "TYPES ARE OUT OF SYNC"
 
 
-def main(type_, project, container, tag, async_, bucket, local, bazel_version, map = None):
-  '''Runs the build. More info in module docstring at the top.
-  '''
+def main(type_,
+         project,
+         container,
+         tag,
+         async_,
+         bucket,
+         local,
+         bazel_version,
+         map=None):
+  """Runs the build. More info in module docstring at the top.
+  """
   type_package_map = TYPE_PACKAGE_MAP
   type_target_map = TYPE_TARGET_MAP
   type_tarball_map = TYPE_TARBALL_MAP
 
-  if map: # Override the map values
+  if map:  # Override the map values
     try:
       map_module = imp.load_source("map", map)
     except IOError as e:
-      print("Error reading map file.\n" ,e)
-    
+      print("Error reading map file.\n", e)
+
     try:
       type_package_map = map_module.TYPE_PACKAGE_MAP
       type_target_map = map_module.TYPE_TARGET_MAP
@@ -168,46 +176,57 @@ def main(type_, project, container, tag, async_, bucket, local, bazel_version, m
   #     subdir/
   #         bazel-toolchains
   #     ...
-  # 
+  #
   # Then if we call build.py with the terminal running in foo,
   # bazel_toolchains_base_dir == "subdir/bazel-toolchains"
   # (This also allows for renaming of the bazel-toolchains folder as the variable
-  # will store the path regardless of the directory names) 
+  # will store the path regardless of the directory names)
   os.chdir(os.path.dirname(__file__))
-  bazel_toolchains_base_dir = os.path.relpath(subprocess.check_output(
-      shlex.split("git rev-parse --show-toplevel")).strip(), project_root)
-  
+  bazel_toolchains_base_dir = os.path.relpath(
+      subprocess.check_output(
+          shlex.split("git rev-parse --show-toplevel")).strip(), project_root)
+
   # We need to start the build from the root of the project, so that we can
   # mount the full root directory (to use bazel builder properly).
   os.chdir(project_root)
+  # We need to run clean to make sure we don't mount local build outputs
+  subprocess.call(["bazel", "clean"])
 
   if local:
     local_build(type_, package, target)
   else:
-   
-    cloud_build(project, container, tag, async_, package, target, bazel_version, bazel_toolchains_base_dir, bucket, tarball)
+
+    cloud_build(project, container, tag, async_, package, target, bazel_version,
+                bazel_toolchains_base_dir, bucket, tarball)
+
 
 def local_build(type_, package, target):
-  '''Runs the build locally. More info in module docstring at the top.
-  '''
+  """Runs the build locally. More info in module docstring at the top.
+  """
   print("Building container locally.")
   subprocess.call(shlex.split("bazel run //{}:{}".format(package, target)))
   print("Testing container locally.")
-  subprocess.call(
-      "bazel test //{}:{}-test".format(package, target).split())
+  subprocess.call("bazel test //{}:{}-test".format(package, target).split())
   print("Tagging container.")
-  subprocess.call(shlex.split("docker tag bazel/{}:{} {}:latest".format(
-      package, target, type_)))
+  subprocess.call(
+      shlex.split("docker tag bazel/{}:{} {}:latest".format(
+          package, target, type_)))
   print(("\n{TYPE}:lastest container is now available to use.\n"
-          "To try it: docker run -it {TYPE}:latest \n").format(TYPE=type_))
+         "To try it: docker run -it {TYPE}:latest \n").format(TYPE=type_))
 
-def cloud_build(project, container, tag, async_, package, target, 
-                bazel_version, bazel_toolchains_base_dir, bucket = None, tarball=None):
-  '''Runs the build in the cloud. More info in module docstring at the top.
-  '''
 
-  # We need to run clean to make sure we don't mount local build outputs
-  subprocess.call(["bazel", "clean"])
+def cloud_build(project,
+                container,
+                tag,
+                async_,
+                package,
+                target,
+                bazel_version,
+                bazel_toolchains_base_dir,
+                bucket=None,
+                tarball=None):
+  """Runs the build in the cloud. More info in module docstring at the top.
+  """
 
   print("Building container in Google Cloud Container Builder.")
 
@@ -229,9 +248,9 @@ def cloud_build(project, container, tag, async_, package, target,
   if not bucket:
     # Gets the yaml relative to the bazel-toolchains root, regardless of what directory it was called from
     # MUST BE UPDATED IF THE YAML FILE IS MOVED
-    config_file = "{}/container/cloudbuild_no_bucket.yaml".format(bazel_toolchains_base_dir)
+    config_file = "{}/container/cloudbuild_no_bucket.yaml".format(
+        bazel_toolchains_base_dir)
     extra_substitution = ""
-
 
   # If script is called within this repo, then we don't need @bazel_toolchains
   # target names (causes infinite symlink chain). If script is called from outside,
@@ -240,32 +259,29 @@ def cloud_build(project, container, tag, async_, package, target,
   if os.path.samefile(bazel_toolchains_base_dir, "."):
     bazel_toolchains_ref = ""
 
-
-  
-
-
   async_arg = ""
   if async_:
     async_arg = "--async"
-  subprocess.call(shlex.split((
-      "gcloud container builds submit . "
-      "--config={CONFIG} "
-      "--substitutions _PROJECT={PROJECT},_CONTAINER={CONTAINER},"
-      "_BAZEL_VERSION={BAZEL_VERSION},"
-      "_BAZEL_TOOLCHAINS_REF={BAZEL_TOOLCHAINS_REF},"
-      "_TAG={TAG},_PACKAGE={PACKAGE},_TARGET={TARGET}{EXTRA_SUBSTITUTION} "
-      "--machine-type=n1-highcpu-32 "
-      "{ASYNC}").format(
-          CONFIG=config_file,
-          PROJECT=project,
-          CONTAINER=container,
-          TAG=tag,
-          BAZEL_TOOLCHAINS_REF=bazel_toolchains_ref,
-          PACKAGE=package,
-          TARGET=target,
-          EXTRA_SUBSTITUTION=extra_substitution,
-          ASYNC=async_arg,
-          BAZEL_VERSION=bazel_version)))
+  subprocess.call(
+      shlex.split(
+          ("gcloud container builds submit . "
+           "--config={CONFIG} "
+           "--substitutions _PROJECT={PROJECT},_CONTAINER={CONTAINER},"
+           "_BAZEL_VERSION={BAZEL_VERSION},"
+           "_BAZEL_TOOLCHAINS_REF={BAZEL_TOOLCHAINS_REF},"
+           "_TAG={TAG},_PACKAGE={PACKAGE},_TARGET={TARGET}{EXTRA_SUBSTITUTION} "
+           "--machine-type=n1-highcpu-32 "
+           "{ASYNC}").format(
+               CONFIG=config_file,
+               PROJECT=project,
+               CONTAINER=container,
+               TAG=tag,
+               BAZEL_TOOLCHAINS_REF=bazel_toolchains_ref,
+               PACKAGE=package,
+               TARGET=target,
+               EXTRA_SUBSTITUTION=extra_substitution,
+               ASYNC=async_arg,
+               BAZEL_VERSION=bazel_version)))
 
 
 def parse_arguments():
@@ -346,14 +362,13 @@ will produce docker locally as {container_type}:latest
   optional.add_argument(
       "-h", "--help", help="print this help text and exit", action="help")
   optional.add_argument(
-      "-m", 
+      "-m",
       "--map",
       help="path to override target map file (can be absolute or relative)."
-            " This allows the building of bazel targets other than the default ones."
-            "(Overrides TYPE_PACKAGE_MAP, TYPE_TARGET_MAP, and TYPE_TARBALL_MAP)", 
+      " This allows the building of bazel targets other than the default ones."
+      "(Overrides TYPE_PACKAGE_MAP, TYPE_TARGET_MAP, and TYPE_TARBALL_MAP)",
       type=str,
       default=None)
-
 
   standalone = parser.add_argument_group("standalone arguments")
 
