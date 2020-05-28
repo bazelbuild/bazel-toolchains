bff3a719c9998de7e82f984b6e9e5eca39094d69
RBE Toolchains Robot <40674274+rbe-toolchains-robot@users.noreply.github.com>
2020-05-28 14:34:04 -0400

    Toolchain configs for marketplace.gcr.io/google/rbe-ubuntu16-04, Bazel 3.2.0 (2020/05/28) (#879)
    * Toolchain configs for marketplace.gcr.io/google/rbe-ubuntu16-04, Bazel 3.2.0 (2020/05/28)
    
    * Update rules_docker sha
    
    * Update bazel_gpg sha
    
    Co-authored-by: Suvanjan Mukherjee <suvanjan@google.com>

--- configs/ubuntu16_04_clang/11.0.0/bazel_3.2.0/cc/cc_wrapper.sh
+++ configs/ubuntu16_04_clang/11.0.0/bazel_3.2.0/cc/cc_wrapper.sh
@@ -1,16 +1,25 @@
-# Copyright 2017 Google Inc. All rights reserved.
+#!/bin/bash
+#
+# Copyright 2015 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Commit sha for bazelbuild/rules_docker."""
+#
+# Ship the environment to the C++ action
+#
+set -eu
 
-version = "e0dee78033e7b632513454d2e8b418e065ab59f5"
+# Set-up the environment
+
+
+# Call the C++ compiler
+/usr/local/bin/clang "$@"
