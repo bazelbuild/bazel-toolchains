82f6f165f44b8e699c878059dbe4bc9da4aef190
alex1545 <42874203+alex1545@users.noreply.github.com>
2020-07-03 09:21:06 -0400

    Toolchain configs for marketplace.gcr.io/google/rbe-ubuntu16-04, Bazel 3.3.1 (2020/07/02) + Fix Bazel tests (#889)
    * Toolchain configs for marketplace.gcr.io/google/rbe-ubuntu16-04, Bazel 3.3.1 (2020/07/02)
    
    * Fix Bazel tests
    
    Co-authored-by: RBE Toolchains Robot <rbe-toolchains-robot@google.com>

--- tests/rbe_external_project/add_rbe_default.sh
+++ tests/rbe_external_project/add_rbe_default.sh
@@ -22,12 +22,12 @@ mv WORKSPACE WORKSPACE.bak
 
 if grep -q rbe_default "WORKSPACE.bak"; then
   # Add content from the txt file right after the line that starts with
-  # 'workspace(' (this will match the 'workspace(name="...")' line only).
+  # 'workspace(name' (this will match the 'workspace(name="...")' line only).
   # Content needs to be added as high up in WORKSPACE as possible so that
   # it cannot be overriden by other declarations.
-  sed '/workspace(/ r ../tests/rbe_external_project/rbe_autoconf_default_targets_no_rbe_default.txt' WORKSPACE.bak > WORKSPACE
+  sed '/workspace(name/ r ../tests/rbe_external_project/rbe_autoconf_default_targets_no_rbe_default.txt' WORKSPACE.bak > WORKSPACE
 else
-  sed '/workspace(/ r ../tests/rbe_external_project/rbe_autoconf_default_targets.txt' WORKSPACE.bak > WORKSPACE
+  sed '/workspace(name/ r ../tests/rbe_external_project/rbe_autoconf_default_targets.txt' WORKSPACE.bak > WORKSPACE
 fi
 
 if [ -f ".bazelrc" ]; then
