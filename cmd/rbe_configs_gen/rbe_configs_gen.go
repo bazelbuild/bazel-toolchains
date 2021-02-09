0b4a7f4ea702a45697aa6ae8ce55c6827285a8f3
Suvanjan Mukherjee <suvanjan@google.com>
2021-02-09 09:58:35 -0500

    Add release workflow & license headers (#938)
    * Add go releaser config
    
    * Minor change
    
    * Remove tags
    
    * Add tag script
    
    * chmod
    
    * fix script
    
    * release config update
    
    * remove tag script
    
    * comments & license headers

--- cmd/rbe_configs_gen/rbe_configs_gen.go
+++ cmd/rbe_configs_gen/rbe_configs_gen.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Binary rbe_configs_gen provides the ability to generate toolchain targets along with a default
 // platform target to configure Bazel to run actions remotely.
 package main
