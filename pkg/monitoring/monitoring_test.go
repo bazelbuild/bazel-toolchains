6221c5862bbc294d9529ff952e58bbd4775f0d4f
Suvanjan Mukherjee <suvanjan@google.com>
2021-02-12 12:22:57 -0500

    Unit tests for report chaining

--- pkg/monitoring/monitoring_test.go
+++ pkg/monitoring/monitoring_test.go
@@ -14,8 +14,114 @@
 //
 package monitoring
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+
+	gax "github.com/googleapis/gax-go/v2"
+	"google.golang.org/genproto/googleapis/api/metric"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+)
+
+type fakeMonitoringClient struct {
+	createTimeSeriesRequests []*monitoringpb.CreateTimeSeriesRequest
+}
+
+func (f *fakeMonitoringClient) CreateMetricDescriptor(_ context.Context, req *monitoringpb.CreateMetricDescriptorRequest, _ ...gax.CallOption) (*metric.MetricDescriptor, error) {
+	return req.MetricDescriptor, nil
+}
+
+func (f *fakeMonitoringClient) DeleteMetricDescriptor(_ context.Context, _ *monitoringpb.DeleteMetricDescriptorRequest, _ ...gax.CallOption) error {
+	return nil
+}
+
+func (f *fakeMonitoringClient) CreateTimeSeries(_ context.Context, req *monitoringpb.CreateTimeSeriesRequest, _ ...gax.CallOption) error {
+	f.createTimeSeriesRequests = append(f.createTimeSeriesRequests, req)
+	return nil
+}
 
 func TestReportToolchainConfigResults(t *testing.T) {
-	t.Logf("hello")
+	testCases := []struct {
+		name         string
+		reportGen    bool
+		reportUpload bool
+		reportTest   bool
+		reportResult bool
+
+		wantCreateTimeSeries int
+	}{
+		{
+			name:                 "GenerationSuccess",
+			reportGen:            true,
+			reportResult:         true,
+			wantCreateTimeSeries: 1,
+		},
+		{
+			// If generation failed, we expect failures to be reported for upload & test as well.
+			// Otherwise, we'll get upload & test jobs aren't running alerts.
+			// will trigger.
+			name:                 "GenerationFailedChainsUploadTestFailures",
+			reportGen:            true,
+			reportResult:         false,
+			wantCreateTimeSeries: 3,
+		},
+		{
+			name:                 "UploadSuccess",
+			reportUpload:         true,
+			reportResult:         true,
+			wantCreateTimeSeries: 1,
+		},
+		{
+			// If upload failed, we expect failures to be reported for test as well. Otherwise, our
+			// internal we'll get test jobs aren't running alerts
+			name:                 "UploadFailedChainsTestFailure",
+			reportUpload:         true,
+			reportResult:         false,
+			wantCreateTimeSeries: 2,
+		},
+		{
+			name:                 "TestSuccess",
+			reportTest:           true,
+			reportResult:         true,
+			wantCreateTimeSeries: 1,
+		},
+		{
+			name:                 "TestFailed",
+			reportTest:           true,
+			reportResult:         false,
+			wantCreateTimeSeries: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			ctx := context.Background()
+			fc := &fakeMonitoringClient{}
+			mc := &Client{
+				mc:        fc,
+				projectID: "fake-project",
+				resetTs:   time.Unix(0, 0),
+			}
+			if tc.reportGen {
+				if err := mc.ReportToolchainConfigsGeneration(ctx, "fake", tc.reportResult); err != nil {
+					t.Errorf("ReportToolchainConfigsGeneration(ctx, fake, %v) failed, got error %v, want nil", tc.reportResult, err)
+				}
+			}
+			if tc.reportUpload {
+				if err := mc.ReportToolchainConfigsUpload(ctx, "fake", tc.reportResult); err != nil {
+					t.Errorf("ReportToolchainConfigsUpload(ctx, fake, %v) failed, got error %v, want nil", tc.reportResult, err)
+				}
+			}
+			if tc.reportTest {
+				if err := mc.ReportToolchainConfigsTest(ctx, "fake", tc.reportResult); err != nil {
+					t.Errorf("ReportToolchainConfigsTest(ctx, fake, %v) failed, got error %v, want nil", tc.reportResult, err)
+				}
+			}
+			if len(fc.createTimeSeriesRequests) != tc.wantCreateTimeSeries {
+				t.Errorf("Unexpected number of time series requests, got %d, want %d.", len(fc.createTimeSeriesRequests), tc.wantCreateTimeSeries)
+			}
+		})
+	}
 }
